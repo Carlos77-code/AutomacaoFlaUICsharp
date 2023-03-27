bb54f591f73fc22a825ab7bf4f651cb3c761a868
Carlos77-code <carloslimafernandes@hotmail.com>
2023-03-27 13:59:43 -0300

    Inicio do projeto de estudo

---
+++ FlaUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.UIA3;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var application = FlaUI.Core.Application.Launch("D:\\carlo\\Projeto\\FlaUIPractice-master\\FlaUIPractice\\FlaUiTests\\Resources\\WinFormsApplication.exe");
+
+            var mainWindow = application.GetMainWindow(new UIA3Automation());
+            ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+
+            mainWindow.FindFirstDescendant(cf.ByName("ContextMenu")).AsButton().Click();
+            mainWindow.FindFirstDescendant(cf.ByAutomationId("TextBox")).AsTextBox().Enter("Carlos");
+            mainWindow.FindFirstDescendant(cf.ByName("Fechar")).AsButton().Click();
+
+        }
+    }
+}
